6e0cc0e49a49b0f02908cc2308beca393ef734dc
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2023-01-29 02:00:06 +0300

    RTK setup: users

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import { store } from './store';
 
 import App from './App';
 import './index.css';
@@ -7,4 +10,8 @@ import './index.css';
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
